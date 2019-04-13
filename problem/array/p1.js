63af09ca7627dbb5f0c5f9dbac0d35b29a2a9391
Ritesh Vishwakarma <ritesh@Riteshs-MacBook-Pro.local>
2019-04-13 15:42:58 +0530

    p1 p2 solved

--- problem/array/p1.js
+++ problem/array/p1.js
@@ -1,1 +1,25 @@
 console.log("How to find the missing number in integer array of 1 to 100?");
+let n=100
+const missing = parseInt(Math.random() * n)
+let data = []
+for (let i = 1; i <= n; i++) {
+  if(missing===i){
+    continue
+  }
+  data.push(i)
+}
+
+function sumOfNumber(params={}) {
+  const {n = 10} = params
+  return n*(n+1)/2
+}
+
+const sumOfArray= data.reduce((acc,cur)=> acc+cur , 0)
+const missingNumber = sumOfNumber({ n }) - sumOfArray
+
+console.log('\n\n=======')
+console.log('INPUT : ', data)
+console.log("System Missing Number : ",missing);
+console.log("Fined Missing Number : ",missingNumber);
+console.log('STATUS : ', missingNumber===missing? 'PASS' : 'FAIL')
+console.log('=======\n\n')
--- problem/array/p2.js
+++ problem/array/p2.js
@@ -1,1 +1,25 @@
 console.log("How to find duplicate number on Integer array?");
+let n=100
+const duplicate = parseInt(Math.random() * n)
+let data = []
+for (let i = 1; i <= n; i++) {
+  if(duplicate===i){
+    data.push(i)
+  }
+  data.push(i)
+}
+
+function sumOfNumber(params={}) {
+  const { n = 10 } = params
+  return n*(n+1)/2
+}
+
+const sumOfArray= data.reduce((acc,cur)=> acc+cur , 0)
+const duplicateNumber = sumOfArray - sumOfNumber({ n })
+
+console.log('\n\n=======')
+console.log('INPUT : ', data)
+console.log("System Duplicate Number : ",duplicate);
+console.log("Fined Duplicate Number : ",duplicateNumber);
+console.log('STATUS : ', duplicateNumber===duplicate? 'PASS' : 'FAIL')
+console.log('=======\n\n')
