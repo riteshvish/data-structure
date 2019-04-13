df5ac6bbfdada61503470eae4bd6bedc4086b239
Ritesh Vishwakarma <ritesh@Riteshs-MacBook-Pro.local>
2019-04-13 17:27:51 +0530

    p31 join array divided by 7 proble

--- problem/array/p31.js
+++ problem/array/p31.js
@@ -1,1 +1,38 @@
-console.log("How to find the missing number in integer array of 1 to 100?");
+console.log("Check Infinity Array Join is dividiby by 7 or not");
+
+// NOTE: WIP
+
+let reminder = 0
+let data=[]
+let outdata=[]
+let testdata=[]
+
+setInterval(()=>{
+  let val = parseInt(Math.random() * 10)
+  reminder = (( reminder * 10 )+ val) % 7
+  data.push(val)
+  outdata.push((reminder===0).toString()[0])
+},50)
+
+setTimeout(function () {
+
+  for (var i = 0; i < outdata.length; i++) {
+    if(outdata[i]==='f'){
+      testdata.push("-")
+      continue
+    }
+    const subArray=data.slice(0,i+1)
+    const sumOfArray= parseInt(subArray.toString().replace(/,/g,''))
+    testdata.push((sumOfArray%7===0)? 'P' : 'F')
+  }
+  console.log('\n\n=======')
+  console.log(data.toString());
+  console.log(outdata.toString());
+  console.log(testdata.toString());
+  console.log('FINAL STATUS : ', testdata.includes("F")? 'FAIL' : 'PASS')
+
+
+  // NOTE: For Test case
+  // parseInt([7,6,5,5,4,5,1,2,3,5,1,9,2,2,2,4,8].toString().replace(/,/g,''))%7
+  process.exit(1)
+}, 1000);
--- problem/array/p5.js
+++ problem/array/p5.js
@@ -1,1 +1,5 @@
 console.log("How to find all pairs on integer array whose sum is equal to given number?");
+const { createRandomArray } = require('../../helper')
+
+let data = [1, -5, 7 ,9 ,2 ,-7 , 1 ]
+const search = data[parseInt(Math.random() * 10)]
